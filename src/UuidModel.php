49cfe15eb15807acccc5768c5df13951d4470d99
Nolan Neustaeter <nolan@flexdealer.com>
2017-11-11 17:29:56 -0800

    switched from trait to extending Model class

---
+++ src/UuidModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Noolan\MyUuid;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UuidModel extends Model
+{
+  public $increments = false;
+  protected $keyType = 'string';
+
+  // convert string UUID to ordered time byte representation
+  public function __call($method, $parameters)
+  {
+    if ($method == 'find') {
+      $parameters[0] = MyUuid::uuidToBin($parameters[0]);
+    }
+
+    return parent::__call($method, $parameters);
+  }
+}
